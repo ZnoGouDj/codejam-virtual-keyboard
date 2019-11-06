81662869387d4439a8c83fae030282248bd0a89d
Egor <znogoud3@gmail.com>
2019-11-06 21:08:25 +0300

    feat: add textarea

--- main.js
+++ main.js
@@ -77,12 +77,105 @@ const englishKb = {
     ]
 };
 
+const russianKb = {
+    rows: [
+        [
+            { key: '`', code: 192, width: 1 },
+            { key: '1', code: 49, width: 1 },
+            { key: '2', code: 50, width: 1 },
+            { key: '3', code: 51, width: 1 },
+            { key: '4', code: 52, width: 1 },
+            { key: '5', code: 53, width: 1 },
+            { key: '6', code: 54, width: 1 },
+            { key: '7', code: 55, width: 1 },
+            { key: '8', code: 56, width: 1 },
+            { key: '9', code: 67, width: 1 },
+            { key: '0', code: 48, width: 1 },
+            { key: '-', code: 189, width: 1 },
+            { key: '=', code: 187, width: 1 },
+            { key: 'backspace', code: 8, width: 2 }
+        ],
+        [
+            { key: 'tab', code: 9, width: 2 },
+            { key: 'й', code: 81, width: 1 },
+            { key: 'ц', code: 87, width: 1 },
+            { key: 'у', code: 69, width: 1 },
+            { key: 'к', code: 82, width: 1 },
+            { key: 'е', code: 84, width: 1 },
+            { key: 'н', code: 89, width: 1 },
+            { key: 'г', code: 85, width: 1 },
+            { key: 'ш', code: 73, width: 1 },
+            { key: 'щ', code: 79, width: 1 },
+            { key: 'з', code: 80, width: 1 },
+            { key: 'х', code: 219, width: 1 },
+            { key: 'ъ', code: 221, width: 1 },
+            { key: "\\", code: 220, width: 1 }
+        ],
+        [
+            { key: 'caps', code: 20, width: 3 },
+            { key: 'ф', code: 65, width: 1 },
+            { key: 'ы', code: 83, width: 1 },
+            { key: 'в', code: 68, width: 1 },
+            { key: 'а', code: 70, width: 1 },
+            { key: 'п', code: 71, width: 1 },
+            { key: 'р', code: 72, width: 1 },
+            { key: 'о', code: 74, width: 1 },
+            { key: 'л', code: 75, width: 1 },
+            { key: 'д', code: 76, width: 1 },
+            { key: 'ж', code: 186, width: 1 },
+            { key: "э", code: 222, width: 1 },
+            { key: 'enter', code: 13, width: 3 }
+        ],
+        [
+            { key: 'shift', code: 16, width: 4 },
+            { key: 'я', code: 90, width: 1 },
+            { key: 'ч', code: 88, width: 1 },
+            { key: 'с', code: 67, width: 1 },
+            { key: 'м', code: 86, width: 1 },
+            { key: 'и', code: 66, width: 1 },
+            { key: 'т', code: 78, width: 1 },
+            { key: 'ь', code: 77, width: 1 },
+            { key: 'б', code: 188, width: 1 },
+            { key: 'ю', code: 190, width: 1 },
+            { key: '\.', code: 191, width: 1 },
+            { key: '⯅', code: 38, width: 1 },
+            { key: 'shift', code: 16, width: 0 }
+        ],
+        [
+            { key: 'ctrl', code: 17, width: 1 },
+            { key: 'win', code: 91, width: 1 },
+            { key: 'alt', code: 18, width: 1 },
+            { key: 'space', code: 32, width: 5 },
+            { key: 'alt', code: 18, width: 1 },
+            { key: 'ctrl', code: 17, width: 1 },
+            { key: 'del', code: 46, width: 1 },
+            { key: '⯇', code: 37, width: 1 },
+            { key: '⯆', code: 40, width: 1 },
+            { key: '⯈', code: 39, width: 0 }
+        ],
+    ]
+};
+
 document.addEventListener('keydown', evt => {
     const buttonEl = document.getElementById(evt.keyCode);
     if (buttonEl) {
         buttonEl.classList.add('active');
     }
+    if (buttonEl.innerText.length <= 1) {
+        add(buttonEl.innerText);
+    }
 })
+
+Element.prototype.remove = function () {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
 
 document.addEventListener('keyup', evt => {
     const buttonEl = document.getElementById(evt.keyCode);
@@ -110,8 +203,25 @@ function renderKeyboard(target, keyboard) {
     });
 }
 
+function renderTextArea() {
+    var div = document.getElementById("keyboard");
+    var input = document.createElement("textarea");
+    input.id = 'textarea';
+    input.name = "post";
+    input.maxLength = "5000";
+    input.cols = "80";
+    input.rows = "40";
+    div.appendChild(input);
+}
+
+function add(text){
+    var TheTextBox = document.getElementById('textarea');
+    TheTextBox.value = TheTextBox.value + text;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const keyboardDiv = document.getElementById('keyboard');
+    renderTextArea();
     if (keyboardDiv) {
         renderKeyboard(keyboardDiv, englishKb);
     }
